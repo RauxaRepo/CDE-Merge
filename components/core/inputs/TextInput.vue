ebc39405b658e90985c3f2a754b894cb68d2c9f6
Victor Montero <victor.montero@prodigious.com>
2021-01-14 13:00:59 -0600

    Remmoving unnecesary class

--- components/core/inputs/TextInput.vue
+++ components/core/inputs/TextInput.vue
@@ -166,7 +166,7 @@ export default {
         newValue = newValue
           .replace(
             /<ul>/g,
-            '<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" class="w92">'
+            '<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center">'
           )
           .replace(/<\/ul>/g, '</table><br/>')
       }
@@ -198,7 +198,7 @@ export default {
         newValue = newValue
           .replace(
             /<ol>/g,
-            '<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" class="w92">'
+            '<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center">'
           )
           .replace(/<\/ol>/g, '</table><br/>')
       }
