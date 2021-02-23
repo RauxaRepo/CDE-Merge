6b804f5712c4620cb86890c96fd8c8de5de7b765
Joshua S. Pacheco <jpacheco@rauxa.com>
2021-02-23 12:13:55 -0500

    adding range slider core input

---
+++ components/core/inputs/Range.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-field :label="label">
+    <b-slider
+    v-model="rangeVal"
+    :custom-formatter="val => val + 'px'"
+    :min="minVal"
+    :max="maxVal"
+    rounded
+    class="range"></b-slider>
+  </b-field>
+</template>
+
+<script>
+export default {
+  props: [
+    "value",
+    "label",
+    "minVal",
+    "maxVal"
+  ],
+  data() {
+    return {
+      rangeVal: 0,
+    }
+  },
+  watch: {
+    rangeVal: function () {
+      this.$emit("input", this.rangeVal)
+    },
+  },
+  mounted: function () {
+    this.rangeVal = this.value
+  },
+}
+</script>
+
+<style>
+.b-slider.range .b-slider-fill {
+  background: #3674AD !important;
+}
+
+.b-tooltip.is-primary .tooltip-content {
+  background: #3674AD !important;
+}
+
+.b-tooltip.is-primary .tooltip-content::before {
+  border-top-color: #3674AD !important;
+}
+</style>
